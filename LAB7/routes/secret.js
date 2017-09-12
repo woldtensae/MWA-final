ae8abd4932967533e2220d639ecf849fb59da237
Aman Woldtensae <woldtensae.aman@gmail.com>
2017-09-12 16:50:40 -0500

    commit7

--- LAB7/routes/secret.js
+++ LAB7/routes/secret.js
@@ -8,21 +8,16 @@ var crypto = require('crypto');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     mongoClient.connect('mongodb://127.0.0.1:27017/test', (err, db) => {
-            
             if(err) throw err;
-            const decipher = crypto.createDecipher('aes256','asaadsaad');
+            var decipher = crypto.createDecipher('aes256','asaadsaad');
             db.collection('homework7').findOne({}, function(err, doc){
                 if(err) throw err;
-                const data = doc.message1;
-                //console.log(data);
-                var dec = decipher.update(String(data),'hex','utf8');
+                const data = doc.message;
+                var dec = decipher.update(data,'hex','utf8');
                 dec += decipher.final('utf8');
-                db.close();
-                res.render('secret', {message: dec});
-                  
-            })
+                res.render('output', {message: dec});
+            });
         });
- 
-});
+    });
 
 module.exports = router;
