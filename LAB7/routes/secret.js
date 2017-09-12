719dd4d03ba2c9406a07d8b871d35d223e451101
Aman <woldtensae.aman@gmail.com>
2017-09-12 07:25:25 -0700

    lab 7 commits

--- LAB7/routes/secret.js
+++ LAB7/routes/secret.js
@@ -1,9 +1,28 @@
+
 var express = require('express');
 var router = express.Router();
+var mongoClient = require('mongodb').MongoClient;
+var crypto = require('crypto');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('secret', {title: 'title'});
+    mongoClient.connect('mongodb://127.0.0.1:27017/test', (err, db) => {
+            
+            if(err) throw err;
+            const decipher = crypto.createDecipher('aes256','asaadsaad');
+            db.collection('homework7').findOne({}, function(err, doc){
+                if(err) throw err;
+                const data = doc.message1;
+                //console.log(data);
+                var dec = decipher.update(String(data),'hex','utf8');
+                dec += decipher.final('utf8');
+                db.close();
+                res.render('secret', {message: dec});
+                  
+            })
+        });
+ 
 });
 
 module.exports = router;
